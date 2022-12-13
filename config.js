07c53d1bf2b296ec18995b8771a527b15fd560e2
ducnh.ous <ducnh.ous@mcredit.com.vn>
2022-12-13 18:00:06 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hà ơiiiiiii!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Tiếp tục để gặp anh Đức nè',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh ❤',
+    btnNo: 'Không, Anh trai à 😢',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ❤',
+    btnReply: 'Gửi cho anh ❤',
+    reply: 'Yêu thì yêu mà không yêu thì yêu ❤❤❤',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón em nhé.',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'https://www.facebook.com/messages/t/100007370475720'
+}
